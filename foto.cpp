b3a886fb6c2912ee5950c904b5abaf70a43e72a7
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-10-17 17:12:23 +0300

    refactor

--- foto.cpp
+++ foto.cpp
@@ -1,4 +1,6 @@
 #include <fstream>
+#define NMAX 100
+#define MMAX 100
 #define ND 4
 using namespace std;
 ifstream fin("foto.in");
@@ -9,29 +11,15 @@ struct punct
 };
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, 1, 0, -1};
+void citesteDateIntrare(int &, int &, int [NMAX+2][MMAX+2]);
+void bordeazaMatricea(int [NMAX+2][MMAX+2], int, int, int);
+int nrPuncteLuminoase(int [NMAX+2][MMAX+2], int, int, int);
 int main()
 {
-	int n, m;
-	fin >> n >> m;
-	int i, j, a[n+2][m+2], nrmax, nrz, nr, inc, sf, d;
-	punct cd[n*m], p, v;
-	for (i = 1; i <= n; i++)
-	{
-		for (j = 1; j <= m; j++)
-		{
-			fin >> a[i][j];
-		}
-	}
-	fin.close();
-	for (j = 0; j <= m+1; j++)
-	{
-		a[0][j] = a[n+1][j] = 1;
-	}
-	for (i = 0; i <= n+1; i++)
-	{
-		a[i][0] = a[i][m+1] = 1;
-	}
-	nrmax = nrz = 0;
+	int n, m, a[NMAX+2][MMAX+2], i, j, nrz, nr, nrmax;
+	citesteDateIntrare(n, m, a);
+	bordeazaMatricea(a, n, m, 1);
+	nrz = nrmax = 0;
 	for (i = 1; i <= n; i++)
 	{
 		for (j = 1; j <= m; j++)
@@ -39,26 +27,7 @@ int main()
 			if (a[i][j] == 0)
 			{
 				nrz++;
-				nr = 1;
-				inc = sf = 0;
-				cd[sf].x = i;
-				cd[sf].y = j;
-				a[i][j] = nrz+1;
-				while (inc <= sf)
-				{
-					p = cd[inc++];
-					for (d = 0; d < ND; d++)
-					{
-						v.x = p.x + dx[d];
-						v.y = p.y + dy[d];
-						if (a[v.x][v.y] == 0)
-						{
-							nr++;
-							cd[++sf] = v;
-							a[v.x][v.y] = nrz+1;
-						}
-					}
-				}
+				nr = nrPuncteLuminoase(a, i, j, nrz);
 				if (nr > nrmax)
 				{
 					nrmax = nr;
@@ -69,5 +38,54 @@ int main()
 	fout << nrmax;
 	fout.close();
 	return 0;
+}
+void citesteDateIntrare(int & n, int & m, int a[NMAX+2][MMAX+2])
+{
+	fin >> n >> m;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			fin >> a[i][j];
+		}
+	}
+	fin.close();
+}
+void bordeazaMatricea(int a[NMAX+2][MMAX+2], int n, int m, int k)
+{
+	for (int j = 0; j <= m+1; j++)
+	{
+		a[0][j] = a[n+1][j] = k;
+	}
+	for (int i = 0; i <= n+1; i++)
+	{
+		a[i][0] = a[i][m+1] = k;
+	}
+}
+int nrPuncteLuminoase(int a[NMAX+2][MMAX+2], int i, int j, int nrz)
+{
+	int nr, inc, sf, d;
+	punct cd[NMAX*MMAX], p, v;
+	nr = 1;
+	inc = sf = 0;
+	cd[sf].x = i;
+	cd[sf].y = j;
+	a[i][j] = nrz+1;
+	while (inc <= sf)
+	{
+		p = cd[inc++];
+		for (d = 0; d < ND; d++)
+		{
+			v.x = p.x + dx[d];
+			v.y = p.y + dy[d];
+			if (a[v.x][v.y] == 0)
+			{
+				nr++;
+				cd[++sf] = v;
+				a[v.x][v.y] = nrz+1;
+			}
+		}
+	}
+	return nr;
 }
 // scor 100
